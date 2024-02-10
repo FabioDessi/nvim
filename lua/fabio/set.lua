a173954cfcd43d5c21d42621b6fe38d7c6b54ddd
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-02-10 04:17:38 +0100

    working config

---
+++ lua/fabio/set.lua
@@ -0,0 +1,19 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.scrolloff = 10
