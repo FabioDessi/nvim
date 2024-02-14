3de432bc5452ea47b28c92d226fd709b68db9772
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-02-14 18:30:27 +0100

    feat: add tokyonight theme

---
+++ lua/fabio/plugins/tokyonight.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/tokyonight.nvim",
+  config = function()
+    require("tokyonight").setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+    })
+
+    require("tokyonight").load()
+  end
+}
