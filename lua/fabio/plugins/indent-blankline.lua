de8baac0f48913b84d13acd2b738ac9c92a6c03c
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-03-29 06:29:05 +0100

    feat: add ibl plugin

---
+++ lua/fabio/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  main = "ibl",
+  opts = {
+    indent = { char = "│" },
+    scope = { enabled = false },
+  },
+
+  config = function (_, opts)
+    require('ibl').setup(opts)
+  end
+}
+
