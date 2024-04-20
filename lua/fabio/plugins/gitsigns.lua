8f663bf4e46246ba65186fdf4320c30c0e527e9b
Fabio Dessì <f.dessi@fabiodessi.dev>
2024-04-20 03:49:55 +0200

    fix: replce : with <cmd>

--- lua/fabio/plugins/gitsigns.lua
+++ lua/fabio/plugins/gitsigns.lua
@@ -4,6 +4,6 @@ return {
   config = function()
     require("gitsigns").setup()
 
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", {})
   end,
 }
